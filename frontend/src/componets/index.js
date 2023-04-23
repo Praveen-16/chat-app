829604c900615077f2d325545bf22f49dd3ac667
praveen <reddypraveen9876@gmail.com>
2023-04-23 16:02:12 +0530

    second commit

---
+++ frontend/src/componets/index.js
@@ -0,0 +1,17 @@
+export { default as ChannelContainer } from './ChannelContainer';
+
+export { default as ChannelListConatiner } from './ChannelListContainer'
+ 
+export { default as ChannelSearch } from './ChannelSearch.jsx';
+export { default as TeamChannelList } from './TeamChannelList.jsx';
+export { default as TeamChannelPreview } from './TeamChannelPreview.jsx';
+export { default as ChannelInner } from './ChannelInner.jsx';
+export { default as CreateChannel } from './CreateChannel.jsx';
+export { default as EditChannel } from './EditChannel.jsx';
+export { default as TeamMessage } from './TeamMessage.jsx';
+export { default as Auth } from './Auth.jsx';
+export { default as UserList } from './UserList.jsx';
+export { default as ResultsDropdown } from './ResultsDropDown.jsx';
+
+
+
